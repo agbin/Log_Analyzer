6bc5330ce04bf74650ad4673817724efae8d4057
agbin <agnieszka.bin@gmail.com>
2020-02-06 01:51:08 +0100

    pytest

--- parser.py
+++ parser.py
@@ -1,153 +1,168 @@
 import datetime
 import sys
 
+# a) find how many word(s) "request" is or are in logline
+def requests_in_loggs(logline, num_req):
+    if "requests" in logline:
+        num_req += 1
+    return num_req
 
-requests = 0
-d = {}
-sizes_of_2xx = 0
-num_of_sizes_of_2xx = 0
+# b) find position of 'response' in log, if response in d dict add 1 to dict[response], if not add response to d dict
+def responses_in_loggs(logline, d):
+    response = logline.split()[13]
+    if response in d:
+        d[response] = d[response] + 1
+    else:
+        d[response] = 1
+    return d
 
+# c)  read 'size of request' in logline, if 'response' of this log begin at 2: add size of it to 'sizes_of_2xx'
+#     and add 1 to num_of_sizes_of_2xx
+def avgsize(logline, d, sizes_of_2xx, num_of_sizes_of_2xx):
+    size = logline.split()[14]  # type str
+    for key in d:
+        if key[0] == '2':
+            sizes_of_2xx += int(size)
+            num_of_sizes_of_2xx += 1
+    return sizes_of_2xx, num_of_sizes_of_2xx
 
-if len(sys.argv) == 6:
-    file = sys.argv[5]
-if len(sys.argv) == 4:
-    file = sys.argv[3]
-if len(sys.argv) == 2:
-    file = sys.argv[1]
+# calculate the difference of two dates and present it in seconds
+def date_diff_in_Seconds(dt2, dt1):
+    timedelta = dt2 - dt1
+    return timedelta.days * 24 * 3600 + timedelta.seconds
 
+# d) count how many requests were send on 1 second (divide the number of requests by the time (in seconds))
+def req_on_sec(start, end, requests):
+    timedelta_in_sec = date_diff_in_Seconds(end, start)
+    return ("%.4f" %(requests / timedelta_in_sec))
 
+# default start date = first date from first line of logfile
+def default_start(first_line):
+    default_start = first_line.split()[5] + "_" + first_line.split()[6]
+    default_start_dt = datetime.datetime.strptime(default_start, "%Y-%m-%d_%H:%M:%S")
+    return default_start_dt
 
-try:
-    with open(file) as f:
+# default end date = last date from first line of logfile
+def default_end(first_line):
+    default_end = first_line.split()[11] + "_" + first_line.split()[12]
+    default_end_dt = datetime.datetime.strptime(default_end, "%Y-%m-%d_%H:%M:%S")
+    return default_end_dt
 
-        first_line = f.readline()                                           # to ignore first line
-
-# default start_date from first line
-        default_start = first_line.split()[5] + "_" + first_line.split()[6]
-        default_start_dt = datetime.datetime.strptime(default_start, "%Y-%m-%d_%H:%M:%S")
-# default end_date from first line
-        default_end = first_line.split()[11] + "_" + first_line.split()[12]
-        default_end_dt = datetime.datetime.strptime(default_end, "%Y-%m-%d_%H:%M:%S")
-
-
-
- # --from, date, --to, date, logfile.log (all argvs are given)
-
-
-        start = default_start_dt
-        end = default_end_dt
-
-        # print("ok start as default_start_dt:", start)
-        # print("ok end as default_end_dt", end)
-        if (sys.argv[1]) == "--from":
-            print("1. ok --from as first argv", sys.argv[2])
+# considering sys.argv and default start and default end, specify start and end
+def start_end(first_logline):
+    start = 0
+    end = 0
+    # if dates are not given, default dates are start, end
+    if len(sys.argv) > 1:
+        if (sys.argv[1]) == "--from" and len(sys.argv) > 2:
+            # if '--from', 'date' is given, set start
             if len(sys.argv[2]) == 19:
                 start = datetime.datetime.strptime(sys.argv[2], "%d-%m-%Y_%H-%M-%S")
             elif len(sys.argv[2]) == 16:
-                r = sys.argv[2] +"-00"
+                r = sys.argv[2] + "-00"
                 start = datetime.datetime.strptime(r, "%d-%m-%Y_%H-%M-00")
+            # if --from date is given and it has wrong format set default start
             elif len(sys.argv[2]) != 19 and len(sys.argv[2]) != 16:
-                print("2. WPISZ POPRAWNY FORMAT DATY: --from %d-%m-%Y_%H-%M-%S, np. --from 01-12-2019_11-33-00")
-        if (sys.argv[1]) == "--to":
-            print("3.. ok --to as first argv", sys.argv[2])
+                print("2. NIEPOPRAWNY FORMAT DATY '--FROM': ZOSTANIE UZYTA DOMYSLNA DATA POCZATKU:", default_start(first_logline))
+        # if --to date is given and --from date is not given, set end
+        if (sys.argv[1]) == "--to" and len(sys.argv) > 2:
+            # print("3.. ok --to as first argv", sys.argv[2])
             # print(sys.argv[1])
             if len(sys.argv[2]) == 19:
                 end = datetime.datetime.strptime(sys.argv[2], "%d-%m-%Y_%H-%M-%S")
             elif len(sys.argv[2]) == 16:
                 r = sys.argv[2] + "-00"
                 end = datetime.datetime.strptime(r, "%d-%m-%Y_%H-%M-00")
+            # if --from date is given and it has wrong format, set default start
             elif len(sys.argv[2]) != 19 and len(sys.argv[2]) != 16:
-                print("4. WPISZ POPRAWNY FORMAT DATY: --from %d-%m-%Y_%H-%M-%S, np. --from 01-12-2019_11-33-00")
-
-        if len(sys.argv) >= 3:
-            if (sys.argv[3]) == "--to":
-
+                print("4. NIEPOPRAWNY FORMAT DATY '--FROM': ZOSTANIE UZYTA DOMYSLNA DATA POCZATKU:", default_start(first_logline))
+        # if there are 1) --from date, 2) --to date, 3) logfile_name given
+        if len(sys.argv) >= 4:
+            if (sys.argv[3]) == "--to" and len(sys.argv) > 4:
                 if len(sys.argv[4]) == 19:
-                    print("5. ok --to as 3. argv as end", sys.argv[4])
+                    # print("5. ok --to as 3. argv as end", sys.argv[4])
                     end = datetime.datetime.strptime(sys.argv[4], "%d-%m-%Y_%H-%M-%S")
                 elif len(sys.argv[4]) == 16:
-                    print("6. ok --to as 3. argv")
-                    r = sys.argv[4] +"-00"
+                    # print("6. ok --to as 3. argv")
+                    r = sys.argv[4] + "-00"
                     end = datetime.datetime.strptime(r, "%d-%m-%Y_%H-%M-00")
-                else:
-                # elif len(sys.argv[4]) != 19 and len(sys.argv[4]) != 16:
-                #     sys.exit("WPISZ POPRAWNY FORMAT DATY '--TO': --to %d-%m-%Y_%H-%M-%S, np. --to 01-12-2019_11-33-00")
-                    print("7. NIEPOPRAWNY FORMAT DATY '--TO': ZOSTANĄ UŻYTA DOMYŚLNA DATA KOŃCOWA")
+            # elif (sys.argv[3]) == "--to" and len(sys.argv) == 4:
+            #     print("NIE ZOSTAŁA PODANA DATA 'END', ZOSTANIE UZYTA DOMYSLNA DATA KONCOWA LOGFILE:", default_end(first_logline))
+            # else:
+            #     print("7. NIEPOPRAWNY FORMAT SŁOWA '--TO': ZOSTANIE UZYTA DATA KONCOWA LOGFILE:", default_end(first_logline))
+    if start == 0:
+        print("ZOSTANIE UZYTA DOMYSLNA DATA POCZATKOWA, --FROM:", default_start(first_logline), ", gdyż wystąpił jeden z przypadków: "
+              "1) NIEPOPRAWNY FORMAT SŁOWA '--from' "
+              "2) BRAK SŁOWA '--from' "
+              "3) NIEPOOPRAWNY FORMAT DATY POCZATKOWEJ"                                                                           
+              "3) DATA POCZATKOWA NIE ZOSTALA PODANA ")
+        start = default_start(first_logline)
+    if end == 0:
+        print("ZOSTANIE UZYTA DOMYSLNA DATA KONCOWA, --to:", default_end(first_logline), ", gdyż wystąpił jeden z przypadków: "
+              "1) NIEPOPRAWNY FORMAT SŁOWA '--to' "
+              "2) BRAK SŁOWA '--to' "
+              "3) NIEPOOPRAWNY FORMAT DATY KONCOWEJ "                                                                           
+              "3) DATA KONCOWA NIE ZOSTALA PODANA ")
+        end = default_end(first_logline)
+    return start, end
 
 
-        print("")
-        print("start:", start)
-        print("end:", end)
-        print("")
-
-        for line in f:
-            data0 = line.split()[8]
-            data = data0[1:]
-            # print(data)
-
-            ts = datetime.datetime.strptime(data, '%d/%b/%Y:%H:%M:%S')
-            if ts > start and ts < end:
-
-#requests:
-                if "requests" in line:
-                    requests += 1
-
-#responses:
-
-                response = line.split()[13]
-                if response in d:
-                    d[response] = d[response] + 1
-                else:
-                    d[response] = 1
-
-#avg size of 2xx responses:
-                size = line.split()[14]  # type str
-                for i in response:
-                    # print(response[0])
-                    if response[0] == "2":
-                        # print("yeah")
-                        # print(size)
-                        sizes_of_2xx += int(size)
-                        num_of_sizes_of_2xx += 1
-
-# requests/sec
-        def date_diff_in_Seconds(dt2, dt1):
-            timedelta = dt2 - dt1
-            return timedelta.days * 24 * 3600 + timedelta.seconds
-
-
-
-        timedelta_in_sec = date_diff_in_Seconds(end, start)
-        requests_sec = requests / timedelta_in_sec
-
-
-
-except IOError:
-    print('''TAKI LOG FILE NIE ISTNIEJE, WPISZ np.: logfile.log /n
-          PRZYKŁADOWE WYWOŁANIA:
-          python3 parser.py --from 01-12-2019_11-23-11 --to 01-12-2019_00-33 logfile.log''')
-
-
-
-print("requests:", requests)
-print("requests/sec:", requests_sec)
-
-# for key in list(d.keys()):
-#     print("responses:", key, ":", d[key])
-#
-# print("responses:", tuple(d.items()))
-
-
-print("responses:", d)
-
-
-
-
-avg_size_of_2xx = sizes_of_2xx / num_of_sizes_of_2xx        # in bytes
-avg_size_of_2xx_Mb = (avg_size_of_2xx * 8) / 1048576
-print("avg size of 2xx responses:", avg_size_of_2xx, "bytes")
-print("avg size of 2xx responses:", avg_size_of_2xx_Mb, "Mb")
+def main(*args):
+    global requests
+    requests = 0
+    global  requests_sec
+    requests_sec = 0
+    d = {}
+    global sizes_of_2xx
+    sizes_of_2xx = 0
+    global num_of_sizes_of_2xx
+    num_of_sizes_of_2xx = 0
+    global avg_size_of_2xx_Mb
+    avg_size_of_2xx_Mb = 0
+    file = sys.argv[-1]
+    if len(sys.argv) == 1:
+        file = "blad"
+    try:
+        with open(file) as f:
+            first_line = f.readline()
+            start, end = start_end(first_line)
+            # results can be caunted if start date is smaller than end date
+            if start < end:
+                print("")
+                print("from:", start)
+                print("to:", end)
+                print("")
+                for line in f:
+                    # date of the individual log (changed on format: datetime)
+                    data0 = line.split()[8]
+                    data = data0[1:]
+                    ts = datetime.datetime.strptime(data, '%d/%b/%Y:%H:%M:%S')
+                    # count on these logfile which dates are between start date end end date
+                    if ts > start and ts < end:
+                        requests = requests_in_loggs(line, requests)
+                        d = responses_in_loggs(line, d)
+                        sizes_of_2xx, num_of_sizes_of_2xx = avgsize(line, d, sizes_of_2xx, num_of_sizes_of_2xx)
+                requests_sec = req_on_sec(start, end, requests)
+            else:
+                print("DATA --FROM MUSI BYC DATA MNIEJSZA OD DATY --TO")
+        # print requests, requests/sec, responses, avg size of 2xx responses (if start date were smaller than end date)
+        if start < end:
+            print("requests:", requests)
+            print("requests/sec:", requests_sec)
+            print("responses:", d)
+            if num_of_sizes_of_2xx == 0:
+                avg_size_of_2xx = 0
+            else:
+                avg_size_of_2xx = sizes_of_2xx / num_of_sizes_of_2xx  # in bytes
+            avg_size_of_2xx_Mb = (avg_size_of_2xx * 8) / 1048576
+            print("avg size of 2xx responses:", avg_size_of_2xx_Mb, "Mb")
+# if name of log does not exist:
+    except IOError:
+        print('''TAKI LOG FILE NIE ISTNIEJE, WPISZ np.: logfile.log  PRZYKLADOWE WYWOLANIE: 
+              python3 parser.py --from 01-12-2019_11-23-11 --to 01-12-2019_00-33 logfile.log''')
+    return requests, requests_sec, d, avg_size_of_2xx_Mb
 
 
 
+main()
 
---
+++ tests.py
@@ -0,0 +1,92 @@
+import pytest
+import parser
+import sys
+import datetime
+
+@pytest.mark.parametrize('logline, number, result',
+                         [
+                             ('Dec 01 11:06:07 app3-test-vm1 gunicorn[53253]: 172.16.3.14 - - [01/Dec/2019:11:06:07 +0100] "GET /internal/user/5fe5aeac-261d-4e2f-9811-c054edda14fa/agenda/2019-12-01/2019-12-02 HTTP/1.1" 200 720 "-" "python-requests/2.22.0" 92048', 0, 1),
+                             ('Dec 01 11:05:45 app3-test-vm1 gunicorn[53253]: 172.16.3.5 - - [01/Dec/2019:11:05:45 +0100] "GET /.well-known/assetlinks.json HTTP/1.0" 404 0 "-" "GoogleAssociationService" 288488', 0, 0)
+                         ]
+                         )
+
+def test_requests_in_loggs(logline, number, result):
+    assert parser.requests_in_loggs(logline, number) == result
+
+
+@pytest.mark.parametrize('logline, d, result',
+                         [
+                             ('Dec 01 11:06:07 app3-test-vm1 gunicorn[53253]: 172.16.3.14 - - [01/Dec/2019:11:06:07 +0100] "GET /internal/user/5fe5aeac-261d-4e2f-9811-c054edda14fa/agenda/2019-12-01/2019-12-02 HTTP/1.1" 200 720 "-" "python-requests/2.22.0" 92048', {}, {'200': 1}),
+                             ('Nov 30 23:04:03 actify3-test-vm1 gunicorn[53253]: 172.16.3.5 - - [30/Nov/2019:23:04:03 +0100] "GET /.well-known/assetlinks.json HTTP/1.0" 404 0 "-" "GoogleAssociationService" 286953', {}, {'404': 1}),
+                         ]
+                         )
+
+def test_responses_in_loggs(logline, d, result):
+    assert parser.responses_in_loggs(logline, d) == result
+
+
+@pytest.mark.parametrize('logline, d, sizes_of_2xx, num_of_sizes_of_2xx, result',
+                         [
+                             ('Dec 01 11:06:07 app3-test-vm1 gunicorn[53253]: 172.16.3.14 - - [01/Dec/2019:11:06:07 +0100] "GET /internal/user/5fe5aeac-261d-4e2f-9811-c054edda14fa/agenda/2019-12-01/2019-12-02 HTTP/1.1" 200 720 "-" "python-requests/2.22.0" 92048', {'200': 1}, 720, 1,  (1440, 2)),
+                             ('Dec 01 11:06:07 app3-test-vm1 gunicorn[53253]: 172.16.3.14 - - [01/Dec/2019:11:06:07 +0100] "GET /internal/user/5fe5aeac-261d-4e2f-9811-c054edda14fa/agenda/2019-12-01/2019-12-02 HTTP/1.1" 200 720 "-" "python-requests/2.22.0" 92048', {'404': 2, '200': 2}, 1440, 2,  (2160, 3))
+
+                         ]
+                         )
+
+def test_avgsize(logline, d, sizes_of_2xx, num_of_sizes_of_2xx, result):
+    assert parser.avgsize(logline, d, sizes_of_2xx, num_of_sizes_of_2xx) == result
+
+def test_date_diff_in_Seconds():
+    result = parser.date_diff_in_Seconds(datetime.datetime(2019, 9, 6, 8, 39, 19), datetime.datetime(2019, 9, 6, 8, 39, 16))
+    assert result == 3
+
+def test_req_on_sec():
+    result = parser.req_on_sec(datetime.datetime(2019, 10, 1, 0, 0, 0), datetime.datetime(2019, 12, 1, 0, 1, 1), 54516)
+    assert result == ("%.4f" %(0.010343687203073887))
+
+def test_default_start():
+    result = parser.default_start('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == datetime.datetime(2019, 9, 6, 8, 39, 18)
+
+def test_default_end():
+    result = parser.default_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == datetime.datetime(2020, 1, 8, 14, 4, 44)
+
+def test_start_end():
+    # all sys.argv (were given (6) arguments)
+    sys.argv = ['parser.py', '--from', '01-12-2019_00-00-00', '--to', '01-12-2019_00-01-01', 'logfile.log']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 12, 1, 0, 0, 0), datetime.datetime(2019, 12, 1, 0, 1, 1))
+    # sys.argv: ['parser.py', '--from', 'date' '--to' 'date'] (were given (5) arguments)
+    sys.argv = ['parser.py', '--from', '01-12-2019_00-00-00', '--to', '01-12-2019_00-01-01']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 12, 1, 0, 0, 0), datetime.datetime(2019, 12, 1, 0, 1, 1))
+    # sys.argv: ['parser.py', '--from', 'date' '--to'] (were given (4) arguments)
+    sys.argv = ['parser.py', '--from', '01-12-2019_00-00-00', '--to']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 12, 1, 0, 0, 0), datetime.datetime(2020, 1, 8, 14, 4, 44))
+    # sys.argv: ['parser.py', '--from', 'date'] (were given (3) arguments)
+    sys.argv = ['parser.py', '--from', 'logfile.log']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 9, 6, 8, 39, 18), datetime.datetime(2020, 1, 8, 14, 4, 44))
+    # sys.argv: ['parser.py', '--to', 'date'] (were given (3) arguments)
+    sys.argv = ['parser.py', '--to', 'logfile.log']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 9, 6, 8, 39, 18), datetime.datetime(2020, 1, 8, 14, 4, 44))
+    # sys.argv: ['parser.py', '--from', 'date'] (were given (2) arguments)
+    sys.argv = ['parser.py', '--from', '01-12-2019_00-00-00', 'logfile.log']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 12, 1, 0, 0, 0), datetime.datetime(2020, 1, 8, 14, 4, 44))
+    # sys.argv: ['parser.py', '--from'] (were given (2) arguments)
+    sys.argv = ['parser.py', '--from']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 9, 6, 8, 39, 18), datetime.datetime(2020, 1, 8, 14, 4, 44))
+    # sys.argv: ['parser.py', '--to'] (were given (2) arguments)
+    sys.argv = ['parser.py', '--to']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 9, 6, 8, 39, 18), datetime.datetime(2020, 1, 8, 14, 4, 44))
+    # sys.argv: 'logfile.log' (were given (1) argument)
+    sys.argv = ['parser.py']
+    result = parser.start_end('-- Logs begin at Fri 2019-09-06 08:39:18 CEST, end at Wed 2020-01-08 14:04:44 CET. --')
+    assert result == (datetime.datetime(2019, 9, 6, 8, 39, 18), datetime.datetime(2020, 1, 8, 14, 4, 44))
+
